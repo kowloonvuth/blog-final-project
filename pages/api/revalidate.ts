7ce1f94a0b0ba8bc0915c789ca30a48396627978
Rany Virakvuth <ranyvirakvuth@gmail.com>
2024-09-08 23:32:30 +0700

    Nextjs Revalidation

---
+++ pages/api/revalidate.ts
@@ -0,0 +1,20 @@
+// https://<your-site.com>/api/validate?secret=<token>
+//http://localhost:3000/api/revalidate?path=/&secret=KowloonVuth
+
+import { error } from "console";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler (
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    if(req.query.secret !== process.env.MY_SECRET_TOKEN) {
+        return res.status(401).json({ message: 'Invalid Token'})
+    }
+
+    const path = req.query.path as string
+
+    await res.revalidate(path)
+    return res.json({ revalidate: true })
+    
+}
